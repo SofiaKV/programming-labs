db376fc31e653c4af7720d2580baaa55328bcc44
SofiaKV <162361442+SofiaKV@users.noreply.github.com>
2024-03-05 23:17:14 +0200

    Add lab3, restructure code

---
+++ lab1/Program3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, -3, 4, 5, -6, 7, -8, 9 };
+
+        int? firstPositive = numbers.FirstOrDefault(x => x > 0);
+        int? lastNegative = numbers.LastOrDefault(x => x < 0);
+
+        Console.WriteLine($"Перший додатній елемент: {firstPositive}");
+        Console.WriteLine($"Останній від'ємний елемент: {lastNegative}");
+    }
+}
