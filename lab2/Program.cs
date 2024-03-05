81857cf473bef338a2ee340bdb4d58eaf4384037
SofiaKV <162361442+SofiaKV@users.noreply.github.com>
2024-03-05 22:53:46 +0200

    Add lab2

---
+++ lab2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace ConsoleApplication4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, double> dictionary = new Dictionary<string, double> {
+                { "item1", 45.50 },
+                { "item2", 35 },
+                { "item3", 41.30 },
+                { "item4", 55 },
+                { "item5", 24 }
+            };
+
+            var sorted = dictionary.OrderByDescending(pair => pair.Value);
+            var topThree = sorted.Take(3);
+
+            foreach (var pair in topThree)
+            {
+                Console.WriteLine($"{pair.Key} {pair.Value}");
+            }
+
+            var json = JsonSerializer.Serialize(topThree.ToDictionary(pair => pair.Key, pair => pair.Value));
+            File.WriteAllText("result.json", json);
+        }
+    }
+}
